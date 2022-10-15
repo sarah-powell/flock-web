0395154e7737f8238b06f62251f1fddf5209207c
sarah-powell <sarahpowell149@gmail.com>
2022-10-15 16:28:13 -0400

    Correct grammar on comp labels

--- src/lib/TimeSlot.svelte
+++ src/lib/TimeSlot.svelte
@@ -1,15 +1,15 @@
 <script>
-  export let value;
-  export let label;
+  export let value = 0;
+  export let label = "undefined";
+
+  // Drop the 's' when needed
+  let labelPlural = value === 1 ? label.slice(0, -1) : label
+
 </script>
 
 <div class="time-slot">
-  <div class="value">
-    {value}
-  </div>
-  <div class="label">
-    {label}
-  </div>
+  <div class="value">{value}</div>
+  <div class="label">{labelPlural}</div>
 </div>
 
 <style>
