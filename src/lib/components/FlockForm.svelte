fa0c5b5f61ae7a8d690fd43e64ac94de908206d2
sarah-powell <sarahpowell149@gmail.com>
2022-12-29 17:00:24 -0500

    Support store on save/edit

--- src/lib/components/FlockForm.svelte
+++ src/lib/components/FlockForm.svelte
@@ -1,7 +1,14 @@
 <script>
-  import { v4 as uuidv5 } from 'uuid';
+  import { onDestroy, onMount } from "svelte";
+  import { goto } from "$app/navigation";
   import { page } from '$app/stores';
-  import { onMount } from "svelte";
+  import { flockStore } from "$lib/stores/FlockStore.js";
+  import { v4 as uuidv5 } from 'uuid';
+
+  let flocks;
+  const unsubscribe = flockStore.subscribe((flockList) => {
+    flocks = flockList;
+  });
 
   const idParam = $page.url.searchParams.get('id');
   let id;
@@ -16,17 +23,32 @@
       date = new Date();
     } else {
       // Editing an existing Flock
-      // let flock = findFlockInStorage(idParam)
-      // if (flock) {
-      //   id = flock.id;
-      //   title = flock.title;
-      //   date = flock.date;
-      // }
+      let flock = flocks.find((f) => idParam === f.id);
+      if (flock) {
+        id = flock.id;
+        title = flock.title;
+        date = flock.date;
+      }
     }
-  })
+  });
+
+  onDestroy(() => {
+    unsubscribe();
+  });
 
   function submitForm() {
+    flockStore.update((flocks) => {
+      // Delete existing flock ID if present
+      flocks = flocks.filter((f) => f.id !== id);
 
+      // Add new flock
+      flocks.push({id, title, date});
+
+      return flocks;
+    });
+
+    // Redirect back to main page
+    goto("/");
   }
 
 </script>
--- src/lib/stores/LocalStorage.js
+++ src/lib/stores/LocalStorage.js
@@ -3,9 +3,11 @@ import { writable } from 'svelte/store'
 /**
  * Creates a store that is driven by localStorage.
  * @param key the localStorage key to store the data under
- * @param initial the initial value to give to storage
+ * @param initial the initial value to give to storage. Should be the raw value,
+ * not stringified JSON.
  */
 export const getLocalStore = (key, initial) => {
+  // Can't access local storage on the server
   const browser = typeof(window) !== 'undefined' && typeof(document) !== 'undefined'
   if (!browser) {
     return writable(initial);
