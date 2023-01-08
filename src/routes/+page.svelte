856987d62fd42e68c4b3eebce745cfac4d423268
sarah-powell <sarahpowell149@gmail.com>
2023-01-08 15:59:03 -0500

    Sort the flocks based on date

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,19 @@
 <script>
   import Flock from '$lib/components/Flock.svelte'
   import { flockStore } from "$lib/stores/FlockStore.js";
+
+  function sortFlocks() {
+    return $flockStore.sort((a, b) => {
+      return a.date - b.date;
+    });
+  }
+
 </script>
 
 <div class="main">
   <a href="/create">+</a>
   <div class="content">
-    {#each $flockStore as flock}
+    {#each sortFlocks() as flock}
       <Flock id={flock.id} title={flock.title} dateTimestamp={flock.date}/>
     {/each}
   </div>
