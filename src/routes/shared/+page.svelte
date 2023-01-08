f30190250bc8496accb8f396336543f6c607a1f6
sarah-powell <sarahpowell149@gmail.com>
2023-01-08 16:53:27 -0500

    Save shared flocks to your flock list

--- src/routes/shared/+page.svelte
+++ src/routes/shared/+page.svelte
@@ -2,6 +2,10 @@
   import { page } from "$app/stores";
   import Flock from "$lib/components/Flock.svelte";
   import PageNav from "$lib/components/PageNav.svelte";
+  import { goto } from "$app/navigation";
+  import { v4 as uuidv5 } from "uuid";
+  import { flockStore } from "$lib/stores/FlockStore.js";
+  import { onDestroy } from "svelte";
 
   const titleParam = $page.url.searchParams.get('t');
   const dateParam = $page.url.searchParams.get('d');
@@ -9,13 +13,41 @@
   let timeStamp = Number(dateParam);
   let date = new Date(timeStamp);
 
+  let flocks;
+  const unsubscribe = flockStore.subscribe((flockList) => {
+    flocks = flockList;
+  });
+
+  onDestroy(() => {
+    unsubscribe();
+  });
+
+  /**
+   * Saves the shared Flock you're viewing to your own Flock list
+   */
+  function saveFlock() {
+    flockStore.update((flocks) => {
+
+      // Add new flock
+      let newFlock = {id: uuidv5(), title: titleParam, dateTimestamp: timeStamp};
+      console.log(newFlock);
+      flocks.push(newFlock);
+
+      return flocks;
+    });
+
+    goto('/');
+  }
+
 </script>
 
 <PageNav>
   <a id="myFlocksButton" href="/">My Flocks</a>
 </PageNav>
 <Flock title={titleParam} dateTimestamp={date} showToolDrawer={false} />
-<button id="#saveButton">Save</button>
+<button id="#saveButton" on:click={saveFlock}>
+  Save
+</button>
 
 <style>
 
